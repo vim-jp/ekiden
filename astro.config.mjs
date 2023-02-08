57a452c18c191c9c905a71797bbc8f7306edd240
Yoichi Takai <contact@peacock0803sz.com>
2023-02-09 03:17:46 +0900

    feat: add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tailwind()],
 });
