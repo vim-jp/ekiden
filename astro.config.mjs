31a6fa27e31a4d0be1d3aeed671a3a9bd5e23887
Yoichi Takai <contact@peacock0803sz.com>
2023-02-10 18:30:34 +0900

    fix: add `site` and `base``

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://peacock0803sz.github.io/ekiden/",
+  base: "/ekiden",
   integrations: [react()]
 });
