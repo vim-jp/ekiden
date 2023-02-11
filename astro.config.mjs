869d143646daeb80f9233514f49ebcb5657576d9
monaqa <cmonaqa@gmail.com>
2023-02-11 13:25:32 +0900

    remove favicon.svg; format config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
 });
+
