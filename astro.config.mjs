397873729709e2632fe438cfad63d2a0521aa946
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-03-11 20:30:05 +0900

    Remove react and calendar. Use timeline layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://vim-jp.org",
   base: "/ekiden",
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
   server:{
       host:true
   }
