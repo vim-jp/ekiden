25bacace3bf11becc6b04b7744514c656afd9140
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-03-19 20:11:14 +0900

    format files

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="astro/client" />
-import 'astro/astro-jsx'
+import "astro/astro-jsx";
 
 declare global {
-    namespace JSX {
-        type Element = HTMLElement
-        type IntrinsicElements = astroHTML.JSX.IntrinsicElements
-    }
+  namespace JSX {
+    type Element = HTMLElement;
+    type IntrinsicElements = astroHTML.JSX.IntrinsicElements;
+  }
 }
