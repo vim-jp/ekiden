6a30e12aeae6b9848ddf7a128b2a3b4fc0d39b2b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-02-20 19:17:03 +0900

    Fix: フォントを固定

--- tests/vrt.spec.ts
+++ tests/vrt.spec.ts
@@ -1,5 +1,15 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.addStyleTag({
+    content: `
+      body {
+        font-family: 'Noto Sans CJK JP'
+      }
+    `,
+  });
+});
+
 test("Visual Regression Test", async ({ page }) => {
   await page.goto(".");
 
