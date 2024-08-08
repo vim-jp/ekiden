acca40a81271002d4c0f6bc9fc3655756fb382d6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 11:13:51 +0100

    feat(eslint): flat config に移行

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import eslint from "@eslint/js";
+
+import tsEslint from "typescript-eslint";
+import tsEslintParser from "@typescript-eslint/parser";
+
+import eslintPluginAstro from "eslint-plugin-astro";
+import astroEslintParser from "astro-eslint-parser";
+
+const astroConfig = tsEslint.config({
+  files: ["*.astro"],
+  extends: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    ...eslintPluginAstro.configs.recommended,
+  ],
+  languageOptions: {
+    parser: astroEslintParser,
+    parserOptions: {
+      parser: tsEslintParser,
+      extraFileExtensions: [".astro"],
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        varsIgnorePattern: "^Props$",
+      },
+    ],
+  },
+});
+
+const tsConfig = tsEslint.config({
+  extends: [
+    tsEslint.configs.eslintRecommended,
+    ...tsEslint.configs.recommended,
+    ...tsEslint.configs.recommendedTypeChecked,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      project: "./tsconfig.json",
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  },
+});
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsConfig,
+  ...astroConfig,
+);
