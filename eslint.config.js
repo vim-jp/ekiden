c4f78e24f0a58f2a992f88460bf10bd72c683555
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-18 22:17:08 +0100

    feat(eslint): add uncoss eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import unocss from "@unocss/eslint-config/flat";
 
 export default [
+  unocss,
   eslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
 ];
