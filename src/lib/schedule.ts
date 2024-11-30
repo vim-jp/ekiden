8402475c581b527aa5b26178fe90cf08832795eb
kyoh86 <me@kyoh86.dev>
2024-11-30 15:07:52 +0900

    feat(schedule): prepare schedule generator

---
+++ src/lib/schedule.ts
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+
+// Schedules must be started on Monday, Wednesday or Friday
+function calcFirstDate(start: dayjs.Dayjs): dayjs.Dayjs {
+  switch (start.day()) {
+    case 0:
+    case 2:
+    case 4:
+      return start.add(1, "d");
+    case 1:
+    case 3:
+    case 5:
+      return start;
+    case 6:
+      return start.add(2, "d");
+  }
+}
+
+/**
+  Generates scheduled dates only on Monday, Wednesday and Friday
+ */
+export function* generate({
+  start,
+}: {
+  start?: dayjs.Dayjs;
+}): Generator<dayjs.Dayjs, void, unknown> {
+  let initialDay = calcFirstDate(start ?? dayjs());
+
+  let cur = initialDay;
+  while (true) {
+    yield cur;
+    cur = cur.add(cur.day() >= 4 ? 3 : 2, "d");
+  }
+}
